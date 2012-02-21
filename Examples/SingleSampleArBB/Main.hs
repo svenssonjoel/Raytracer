8f7002d03af8991c3b2db76806078e7fb67751c6
Joel Svensson <svenssonjoel@yahoo.se>
2012-02-21 22:07:27 +0100

    hacking on ray-triangle intersection code

--- Examples/SingleSampleArBB/Main.hs
+++ Examples/SingleSampleArBB/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-} 
 {- 2012 Joel Svensson -} 
 
 import Trace.RGB
@@ -42,8 +43,8 @@ arbbConvert src targ = do
     map_ body [outp] [inp] 
   
   -- elements, not bytes. (size) 
-  inb <- createDenseBinding_ (castPtr src) 1 [500*500*3{-size-}] [4{-pitch-}] 
-  outb <- createDenseBinding_ (castPtr targ) 1 [500*500*3] [1] 
+  inb <- createDenseBinding_ (castPtr src) 1 [10*10*3{-size-}] [4{-pitch-}] 
+  outb <- createDenseBinding_ (castPtr targ) 1 [10*10*3] [1] 
   
   gin <- createGlobal_ d_f "input" inb
   gout <- createGlobal_ d_i "output" outb 
@@ -58,19 +59,19 @@ arbbConvert src targ = do
 genArbbDotProd = 
   do 
     s_f <- getScalarType_ ArbbF32
-    s_d <- getDenseType_ s_f 1  
-    funDef_ "dotProd" [s_f] [s_d,s_d] $ \ [out] [v1,v2] -> do 
-      prods <- createLocal_ s_d "prods" 
+    d_f <- getDenseType_ s_f 1  
+    funDef_ "dotProd" [s_f] [d_f,d_f] $ \ [out] [v1,v2] -> do 
+      prods <- createLocal_ d_f "prods" 
       op_ ArbbOpMul [prods] [v1,v2] 
       opDynamic_ ArbbOpAddReduce [out] [prods]
     
 genArbbCrossProd =   
   do 
     s_f <- getScalarType_ ArbbF32
-    s_d <- getDenseType_ s_f 1 
-    funDef_ "crossProd" [s_d] [s_d,s_d] $ \ [r] [v1,v2] -> do 
-      lprods <- createLocal_ s_d "leftProds" 
-      rprods <- createLocal_ s_d "rightProds" 
+    d_f <- getDenseType_ s_f 1 
+    funDef_ "crossProd" [d_f] [d_f,d_f] $ \ [r] [v1,v2] -> do 
+      lprods <- createLocal_ d_f "leftProds" 
+      rprods <- createLocal_ d_f "rightProds" 
       
       one    <- isize_ 1 
       two    <- isize_ 2
@@ -81,7 +82,88 @@ genArbbCrossProd =
       opDynamic_ ArbbOpRotateReverse [v2] [v2,one] 
       op_ ArbbOpMul [rprods] [v1,v2] 
       op_ ArbbOpSub [r] [lprods,rprods]
+        
+genRayTrigIntersect =       
+  do 
+    s_i <- getScalarType_ ArbbI32
+    s_b <- getScalarType_ ArbbBoolean
+    s_f <- getScalarType_ ArbbF32
+    d_f <- getDenseType_  s_f 1   -- 3d vectors and points
+    
+    crossP <- genArbbCrossProd
+    dotP   <- genArbbDotProd
+    
+    funDef_ "trigIntersect" [{-s_i,-}d_f] [d_f,d_f,d_f,d_f,d_f,d_f,d_f] $ \ [{-hit,-}color] [o,d,t0,t1,t2,tcol,black] -> do 
+      e0 <- createLocal_ d_f "e0"
+      e1 <- createLocal_ d_f "e1"
+      e3 <- createLocal_ d_f "e3"
+      e4 <- createLocal_ d_f "e4" 
+      e5 <- createLocal_ d_f "e5"
       
+      denom <- createLocal_ s_f "denom" 
+      beta  <- createLocal_ s_f "beta"
+      gamma <- createLocal_ s_f "gamma" 
+      tval  <- createLocal_ s_f "tval" 
+      cond1 <- createLocal_ s_b "c1"
+      cond2 <- createLocal_ s_b "c2"
+      cond3 <- createLocal_ s_b "c3"
+      
+      zero <- float32_ 0
+      one  <- float32_ 1
+      
+      true <- int32_ 1
+      false <- int32_ 0
+      
+      op_ ArbbOpSub [e0] [t0,t1] 
+      op_ ArbbOpSub [e1] [t0,t2] 
+      op_ ArbbOpSub [e4] [t0,o]  --t0 - rayOrigin
+      
+      call_ crossP [e3] [e1,d] 
+      
+      call_ dotP   [denom] [e0,e3] 
+      
+      op_ ArbbOpDiv [denom] [one,denom] 
+      
+      call_ dotP   [beta] [e4,e3] 
+      op_ ArbbOpMul [beta] [beta,denom] 
+      
+      call_ crossP [e5] [e0,e4] 
+      
+      call_ dotP [gamma] [e5,d]      
+      op_ ArbbOpMul [gamma] [gamma,denom]
+      
+      call_ dotP [tval] [e5,e1] 
+      op_ ArbbOpSub [tval] [zero,tval]
+      op_ ArbbOpMul [tval] [tval,denom]
+      
+      op_ ArbbOpLeq [cond1] [beta,zero] 
+      op_ ArbbOpGeq [cond2] [beta,one] 
+      op_ ArbbOpMax [cond3] [cond1,cond2] -- Max used as or. possible ? 
+      
+      if_ cond3 
+        (do 
+          -- op_ ArbbOpCopy [hit] [false]   
+          op_ ArbbOpCopy [color] [black]
+        ) 
+        (do 
+          op_ ArbbOpLeq [cond1] [gamma,zero]   
+          bpg <- createLocal_ s_f "beta + gamma" 
+          op_ ArbbOpAdd [bpg] [beta,gamma] 
+          op_ ArbbOpGeq [cond2] [bpg,one] 
+          op_ ArbbOpMax [cond3] [cond1,cond2] 
+          if_ cond3 
+            (do 
+              --op_ ArbbOpCopy [hit] [false]   
+              op_ ArbbOpCopy [color] [black]
+            )
+            (do 
+              --op_ ArbbOpCopy [hit] [true]   
+              op_ ArbbOpCopy [color] [tcol]
+            )
+        )
+          
+  
+        
 bindToVar1D ptr s p t nom = 
   do 
    t' <- getScalarType_ t 
@@ -93,8 +175,10 @@ bindToVar1D ptr s p t nom =
 
 ----------------------------------------------------------------------------
 -- Main
-main = 
+main = do
   -- Test
+   
+  {- 
   withArray [1.0,1.0,1.0 :: Float] $ \ v1 -> 
     withArray [2.0,3.0,4.0 :: Float] $ \ v2 -> 
       allocaArray (3*4) $ \ r -> 
@@ -102,6 +186,7 @@ main =
           arbbSession $ 
            do  
              f1 <- genArbbCrossProd
+             f2 <- genRayTrigIntersect
 
              var1 <- bindToVar1D v1 (3 {-elements-}) 4 ArbbF32 "v1" 
              var2 <- bindToVar1D v2 3 4 ArbbF32 "v2" 
@@ -111,7 +196,90 @@ main =
           result <- (peekArray 3 r :: IO [Float])
           putStrLn $ show result    
           putStrLn $ show (Vector3 1 1 1 `crossProd` Vector3 2 3 4)
+  -}         
+  {-
+  withArray [0.0,0.0,-1.0 :: Float] $ \ d ->  
+   withArray [250.0,250.0,0.0 :: Float] $ \ o -> 
+    withArray [300.0,600.0,-800.0 :: Float] $ \ p0 -> 
+     withArray [0.0,100.0,-1000.0 :: Float] $ \ p1 -> 
+      withArray [450.0,20.0,-1000.0 :: Float] $ \ p2 -> 
+       withArray [0.8,0.2,0.2 :: Float] $ \ c -> 
+        withArray [0.0,0.0,0.0 :: Float] $ \ black -> 
+         allocaArray (3*4) $ \ (r :: Ptr Float) -> 
+           do 
+            arbbSession $ 
+             do  
+               f1 <- genRayTrigIntersect
+
+               d' <- bindToVar1D d (3 {-elements-}) 4 ArbbF32 "d" 
+               o' <- bindToVar1D o 3 4 ArbbF32 "o" 
+              
+               p0' <- bindToVar1D p0 3 4 ArbbF32 "p0" 
+               p1' <- bindToVar1D p1 3 4 ArbbF32 "p1" 
+               p2' <- bindToVar1D p2 3 4 ArbbF32 "p2" 
+               c' <- bindToVar1D  c  3 4 ArbbF32 "c" 
+               black' <- bindToVar1D  black  3 4 ArbbF32 "c" 
+               
+              
+               r' <- bindToVar1D r   3 4 ArbbF32 "r" 
+              
+               --sty  <- getScalarType_ ArbbI32
+               --g    <- createGlobal_nobind_ sty "res" --outb
+               --b    <- variableFromGlobal_ g 
           
+               execute_ f1 [r'] [o',d',p0',p1',p2',c',black'] 
+               result <- liftIO$ (peekArray 3 r :: IO [Float])
+               --bresult :: Int32 <- readScalar_ b 
+               liftIO$ putStrLn $ show result -- ++ " " ++ show bresult
+  -}
+  
+  
+  -- Trying to figure out if rayTrigIntersection works
+  -- (Extremely slow running test) 
+  withArray [0.0,0.0,-1.0 :: Float] $ \ d ->  
+    withArray [300.0,600.0,-800.0 :: Float] $ \ p0 -> 
+     withArray [0.0,100.0,-1000.0 :: Float] $ \ p1 -> 
+      withArray [450.0,20.0,-1000.0 :: Float] $ \ p2 -> 
+       withArray [0.8,0.2,0.2 :: Float] $ \ c -> 
+        withArray [0.0,0.0,0.0 :: Float] $ \ black -> 
+           do 
+             
+             rgbs <- sequence
+               [do
+                 withArray [x,(y+98),0::Float]  $ \ o -> 
+                  allocaArray (3*4) $ \ (r :: Ptr Float) ->  
+                   arbbSession $ 
+                    do  
+                
+                      f1 <- genRayTrigIntersect
+
+                      d' <- bindToVar1D d (3 {-elements-}) 4 ArbbF32 "d" 
+                      o' <- bindToVar1D o 3 4 ArbbF32 "o" 
+              
+                      p0' <- bindToVar1D p0 3 4 ArbbF32 "p0" 
+                      p1' <- bindToVar1D p1 3 4 ArbbF32 "p1" 
+                      p2' <- bindToVar1D p2 3 4 ArbbF32 "p2" 
+                      c'  <- bindToVar1D  c  3 4 ArbbF32 "c" 
+                      black' <- bindToVar1D  black  3 4 ArbbF32 "b" 
+               
+                      r' <- bindToVar1D r 3 4 ArbbF32 "r" 
+              
+          
+                      execute_ f1 [r'] [o',d',p0',p1',p2',c',black'] 
+              
+                      result <- liftIO$ (peekArray 3 r :: IO [Float])
+                      liftIO$ putStrLn "*******************************************"
+                      return result 
+               | y <- [0..9], x <- [0..9]
+               ]   
+             putStrLn $ show $ length (concat rgbs)
+             withBinaryFile "test.raw" WriteMode $ \ handle -> 
+               withArray (concat rgbs) $ \ arr -> 
+                 allocaArray (10*10*3) $ \ bytes -> do 
+                  arbbSession (arbbConvert (castPtr arr) bytes) 
+                  hPutBuf handle bytes (10*10*3) 
+  
+         
           
   
   --withBinaryFile "test.raw" WriteMode $ \ handle -> 
