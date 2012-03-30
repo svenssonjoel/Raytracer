199909d5d1b25e668a68073189c7ed6125ee17bb
Joel Svensson <svenssonjoel@yahoo.se>
2012-03-30 14:43:36 +0200

    -

--- Trace/ONB.hs
+++ Trace/ONB.hs
@@ -4,4 +4,9 @@ module Trace.ONB where
 
 import Trace.Vector3
 
-data ONB = ONB 
+data ONB = ONB {onbU :: Vector3, 
+                onbV :: Vector3, 
+                onbW :: Vector3}
+           
+
+epsilon =  0.01 
